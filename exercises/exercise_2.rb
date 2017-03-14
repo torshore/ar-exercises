c8728c098b941aae7d3ccc973d678ba14c7c8dec
Tory Shoreman <toryshoreman@gmail.com>
2017-03-14 23:19:58 +0000

    Committing Exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.annual_revenue = 2
+@store1.save
+
 # Your code goes here ...
