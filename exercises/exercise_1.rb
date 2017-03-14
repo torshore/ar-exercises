6a449cca3a4cfd47449e23f6e620a2e66280fe86
Tory Shoreman <toryshoreman@gmail.com>
2017-03-14 23:15:25 +0000

    Solution to Exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,29 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+burnaby = Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  womens_apparel: true,
+  mens_apparel: true
+  )
+
+
+richmond = Store.create(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  womens_apparel: true,
+  mens_apparel: false
+  )
+
+gastown = Store.create(
+  name: "Gastown",
+  annual_revenue: 190000,
+  womens_apparel: false,
+  mens_apparel: true
+  )
+
+puts Store.count
+
+
+
